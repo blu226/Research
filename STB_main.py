659d4c59dad365cd33ec51e280c0a3550faa3bef
Vijay <vjsah27@gmail.com>
2018-03-05 13:49:16 -0500

    Modified LLC path

--- STB_main.py
+++ STB_main.py
@@ -18,6 +18,9 @@ ADJ_E.fill(math.inf)
 ADJ_MSG    = numpy.empty(shape = (V, V, S, T, len(M))) # Adjacency matrix that holds message transmission delay for each message for each node pair
 ADJ_MSG.fill(math.inf)
 
+LINK_EXISTS = numpy.empty(shape=(4, 4, 2, 5, 5))
+LINK_EXISTS.fill(math.inf)
+
 # MODULES
 
 tau = computeTau()                              # Get the discrete time interval period
@@ -25,18 +28,20 @@ specBW = getSpecBW(specBW, V, S, T)                     # Get the dynamic spectr
 
 # ADJ = initializeADJ(ADJ, V, S, T, tau, specBW)
 # printADJ(ADJ, V, S, T, tau)
+LINK_EXISTS = createLinkExistenceADJ(LINK_EXISTS)
+# printADJ(LINK_EXISTS, V, S, T, tau)
 
-ADJ_MSG = computeADJ_MSG(specBW, ADJ_MSG, V, S, T, M, tau)
-printADJ_MSG(ADJ_MSG, V, S, T, M, tau)
+ADJ_MSG = computeADJ_MSG(specBW, ADJ_MSG, LINK_EXISTS, V, S, T, M, tau)
+# printADJ_MSG(ADJ_MSG, V, S, T, M, tau)
 
 # ADJ_E = initializeADJ2(ADJ_E, V, S, T, tau, specBW, specBW)      #Initialize the 5D adjacency matrix
 # print("ADJ_E MATRIX")
 # printADJ(ADJ_E, V, S, T, tau)
 #
-# LLC_Path, Parent, Spectrum = LLC_PATH_ADJ(ADJ, ADJ_MSG, V, S, T, M, tau)
+LLC_Path, Parent, Spectrum = LLC_PATH_ADJ(ADJ_MSG, V, S, T, M, tau)
 #
-# print("Least Latency Cost Path")
-# printADJ_4D(LLC_Path, V, T, M)
+print("Least Latency Cost Path")
+printADJ_4D(LLC_Path, V, T, M)
 
 # print("Parent")
 # print4d(Parent)
