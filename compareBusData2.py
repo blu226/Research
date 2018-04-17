0b9a46dedd2fe4ad0cc1fe3f6e454cdf2e5c2690
Brian Luciano <blu226@g.uky.edu>
2018-04-17 15:16:08 -0400

    Compute Similarity Ratio for all files

--- compareBusData2.py
+++ compareBusData2.py
@@ -8,62 +8,77 @@ folders = findfiles(directory)
 folders.sort()
 folderLen = len(folders)
 
-file1 = directory + "/" + str(folders[0])  #2007-10-23
-file2 = directory + "/" + str(folders[14]) #2007-11-06
+for first_file in range(folderLen - 1):
+    for second_file in range(folderLen - 1):
+        if( first_file != second_file):
 
-with open(directory +"/" + str(folders[0]), "r") as f:
-    day1_lines = f.readlines()[1:]
+            file1 = directory + "/" + str(folders[first_file])  #2007-10-23
+            file2 = directory + "/" + str(folders[second_file]) #2007-11-06
 
-with open(directory +"/" + str(folders[14]), "r") as f:
-    day2_lines = f.readlines()[1:]
+            with open(file1, "r") as f:
+                day1_lines = f.readlines()[1:]
 
-rowSimilar = np.zeros(shape=(30, 28))
-numRows = np.zeros(shape=(30,28))
+            with open(file2, "r") as f:
+                day2_lines = f.readlines()[1:]
 
-common_line_len = min(len(day1_lines), len(day2_lines))
+            day1_num_buses_loop = len(day1_lines[1].split(","))
+            day2_num_buses_loop = len(day2_lines[1].split(","))
 
-day1_bus = -1
+            day1_num_buses = int((day1_num_buses_loop - 1)/2)
+            day2_num_buses = int((day2_num_buses_loop - 1)/2)
 
-for i in range(1, 61, 2):
+            rowSimilar = np.zeros(shape=(day1_num_buses, day2_num_buses))
+            numRows = np.zeros(shape=(day1_num_buses, day2_num_buses))
 
-    day1_bus += 1
-    day2_bus = -1
+            common_line_len = min(len(day1_lines), len(day2_lines))
 
-    for j in range(1, 57, 2):
+            day1_bus = -1
 
-        day2_bus += 1
+            for i in range(1, day1_num_buses_loop, 2):
 
-        for ind in range(common_line_len):
+                day1_bus += 1
+                day2_bus = -1
+
+                for j in range(1, day2_num_buses_loop, 2):
+
+                    day2_bus += 1
+
+                    for ind in range(common_line_len):
 
 
-            day1_line = day1_lines[ind].strip()
-            day1_line_arr = day1_line.split(",")
+                        day1_line = day1_lines[ind].strip()
+                        day1_line_arr = day1_line.split(",")
 
-            day2_line = day2_lines[ind].strip()
-            day2_line_arr = day2_line.split(",")
+                        day2_line = day2_lines[ind].strip()
+                        day2_line_arr = day2_line.split(",")
 
-            X1 = day1_line_arr[i]
-            Y1 = day1_line_arr[i+1]
+                        X1 = day1_line_arr[i]
+                        Y1 = day1_line_arr[i+1]
 
-            X2 = day2_line_arr[j]
-            Y2 = day2_line_arr[j+1]
+                        X2 = day2_line_arr[j]
+                        Y2 = day2_line_arr[j+1]
 
 #            print("X1: " + str(X1) + " Y1: " + str(Y1) + " X2: " + str(X2) + " Y2: " + str(Y2))
 
-            if (X1 != "-" and X1 != "0.0" and X2 != "-" and X2 != "0.0"):
+                        if (X1 != "-" and X1 != "0.0" and X2 != "-" and X2 != "0.0"):
 
-                numRows[day1_bus][day2_bus] += 1
-                dist = funHaversine(float(X1), float(Y1), float(X2), float(Y2))
+                            numRows[day1_bus][day2_bus] += 1
+                            dist = funHaversine(float(X1), float(Y1), float(X2), float(Y2))
 #                print(dist)
-                if dist < .1 and dist > 0:
-                    rowSimilar[day1_bus][day2_bus] += 1
+                            if dist < .1 and dist > 0:
+                                rowSimilar[day1_bus][day2_bus] += 1
 
-print("i\tj\tNumRows\tSimilarity")
+            print("\n---------------------------------------------------------------\n")
+            print("Days: " + str(folders[first_file]) + " and " + str(folders[second_file]))
+            print("i\tj\tNumRows\tSimilarity")
 
-for i in range(30):
-    for j in range(28):
+            for i in range(day1_num_buses):
+                for j in range(day2_num_buses):
 
-        if numRows[i][j] != 0 and numRows[i][j] > 100:
-            ratio = (rowSimilar[i][j]/numRows[i][j]) * 100
-            if ratio > 10:
-                print(str(i) + "\t" + str(j) + "\t" + str(numRows[i][j]) + "\t" + str(ratio) + "%")
+                    if numRows[i][j] != 0 and numRows[i][j] > 100:
+                        ratio = (rowSimilar[i][j]/numRows[i][j]) * 100
+                        if ratio > 50:
+                            print(str(i) + "\t" + str(j) + "\t" + str(numRows[i][j]) + "\t" + str(ratio) + "%")
+
+
+
