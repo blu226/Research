cd2657314e15845e929bbd3ab83ecbc434c8d969
Brian Luciano <blu226@g.uky.edu>
2018-04-26 13:16:29 -0400

    Create DataMules folder

--- compareBusData2.py
+++ compareBusData2.py
@@ -1,12 +1,68 @@
 from map_plot import findfiles
 from computeHarvesine import *
 import numpy as np
+import os
+import shutil
 
 directory = "DateWiseRoutes_CSV"
 
 folders = findfiles(directory)
 folders.sort()
 folderLen = len(folders)
+
+def moveFiles(day1, day2, similarity, day1_name, day2_name):
+
+    day1_chosen = []
+    day2_chosen = []
+
+    while len(day1) != 0:
+        maxIndex = -1
+        maxSim = 0
+
+        for i in range(len(day1)):
+            if similarity[i] > maxSim:
+                maxIndex = i
+                maxSim = similarity[i]
+
+        if day1[maxIndex] not in day1_chosen and day2[maxIndex] not in day2_chosen:
+            day1_chosen.append(day1[maxIndex])
+            day2_chosen.append(day2[maxIndex])
+
+        day1.pop(maxIndex)
+        day2.pop(maxIndex)
+        similarity.pop(maxIndex)
+
+    day1_path = "DateWiseRoutes/" + day1_name[:10]
+    day1_folder = findfiles(day1_path)
+    day2_path = "DateWiseRoutes/" + day2_name[:10]
+    day2_folder = findfiles(day2_path)
+
+    destFolder = "DataMules/" + day1_name[:10] + "_" + day2_name[:10]
+    if not os.path.exists(destFolder):
+        os.makedirs(destFolder)
+
+    destFolder1 = destFolder + "/" + day1_name
+    if not os.path.exists(destFolder1):
+        os.makedirs(destFolder1)
+
+    destFolder2 = destFolder + "/" + day2_name
+    if not os.path.exists(destFolder2):
+        os.makedirs(destFolder2)
+    print(day1_chosen)
+    for i in range(len(day1_chosen)):
+        day1_file = day1_folder[day1_chosen[i]]
+        day1_filePath = day1_path + "/" + day1_file
+        day2_file = day2_folder[day2_chosen[i]]
+        day2_filePath = day2_path + "/" + day2_file
+
+        day1_dest = destFolder1 + "/" + str(i) + ".txt"
+        day2_dest = destFolder2 + "/" +str(i) + ".txt"
+
+        shutil.copyfile(day1_filePath, day1_dest)
+        shutil.copyfile(day2_filePath, day2_dest)
+
+
+
 
 
 for first_file in range(folderLen - 1):
@@ -21,6 +77,10 @@ for first_file in range(folderLen - 1):
 
             with open(file2, "r") as f:
                 day2_lines = f.readlines()[1:]
+
+            day1_buses = []
+            day2_buses = []
+            similarity = []
 
             day1_num_buses_loop = len(day1_lines[1].split(","))
             day2_num_buses_loop = len(day2_lines[1].split(","))
@@ -66,30 +126,32 @@ for first_file in range(folderLen - 1):
                             numRows[day1_bus][day2_bus] += 1
                             dist = funHaversine(float(X1), float(Y1), float(X2), float(Y2))
 #                print(dist)
-                            if dist < .12 and dist > 0:
+                            if dist < .2 and dist > 0:
                                 rowSimilar[day1_bus][day2_bus] += 1
 
             f = open("Similarity_Ratios.txt", "a")
 
-            print("\n---------------------------------------------------------------\n")
-            print("Days: " + str(folders[first_file]) + " and " + str(folders[second_file]))
-            print("i\tj\tNumRows\tSimilarity")
+            # print("---------------------------------------------------------------\n")
+            # print("Days: " + str(folders[first_file]) + " and " + str(folders[second_file]))
+            # print("i\tj\tNumRows\tSimilarity")
 
-            f.write("\n---------------------------------------------------------------\n")
-            f.write("Days: " + str(folders[first_file]) + " and " + str(folders[second_file]) + "\n")
-            f.write("i\tj\tNumRows\tSimilarity\n")
-
+            # f.write("---------------------------------------------------------------\n")
+            # f.write("Days: " + str(folders[first_file]) + " and " + str(folders[second_file]) + "\n")
+            # f.write("i\tj\tNumRows\tSimilarity\n")
 
             for i in range(day1_num_buses):
                 for j in range(day2_num_buses):
-
                     if numRows[i][j] != 0 and numRows[i][j] > 100:
                         ratio = (rowSimilar[i][j]/numRows[i][j]) * 100
                         if ratio > 50:
-                            print(str(i) + "\t" + str(j) + "\t" + str(numRows[i][j]) + "\t" + str(ratio) + "%")
-                            f.write(str(i) + "\t" + str(j) + "\t" + str(numRows[i][j]) + "\t" + str(ratio) + "%\n")
+                            # print(str(i) + "\t" + str(j) + "\t" + str(numRows[i][j]) + "\t" + str(ratio) + "%")
+                            # f.write(str(i) + "\t" + str(j) + "\t" + str(numRows[i][j]) + "\t" + str(ratio) + "%\n")
 
+                            day1_buses.append(i)
+                            day2_buses.append(j)
+                            similarity.append(ratio)
 
             f.close()
 
 
+            moveFiles(day1_buses, day2_buses, similarity, str(folders[first_file]), str(folders[second_file]))
