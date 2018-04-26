7f8da9146140e2550067f8038fc6b51f742efc29
Vijay <vjsah27@gmail.com>
2018-04-25 22:32:14 -0400

    handled issues with run simulation.

--- run_simulation.py
+++ run_simulation.py
@@ -11,6 +11,8 @@ if generate_files == "Y":
     for run in range(1, int(number_of_runs) + 1):
         print("Round: " + str(run))
 
+        S = [0, 1, 2, 3]
+        path_to_folder = "Bands/" + str(run) + "/ALL/"
         link_exists_folder = "Bands/" + str(run) +"/"
         lex_data_directory = "Lexington/" + str(run) +"/"
         lex_data_directory_day = "Lexington/" + str(run) + "/Day1/"
@@ -20,9 +22,12 @@ if generate_files == "Y":
 
         with open("constants.py", "w") as f:
             for line in lines:
-                if ("link_exists_folder" not in line and "lex_data_directory" not in line):
+                if ("path_to_folder" not in line) and ("S = " not in line) \
+                        and ("link_exists_folder" not in line) and ("lex_data_directory" not in line):
                     f.write(line)
 
+            f.write("path_to_folder = '" + str(path_to_folder) + "'\n")
+            f.write("S = " + str(S) + "\n")
             f.write("lex_data_directory = '" + str(lex_data_directory) + "'\n")
             f.write("lex_data_directory_day = '" + str(lex_data_directory_day) + "'\n")
             f.write("link_exists_folder = '" + str(link_exists_folder) + "'\n")
@@ -38,7 +43,7 @@ run_simulation = input("Do you want to run the simulation?Y/N  ")
 
 #TODO: Run the simulation
 if run_simulation == "Y":
-    for ind in range(0, 5):
+    for ind in range(0, 1):
         for run in range(1, int(number_of_runs) + 1):
 
             if ind == 0:
@@ -54,31 +59,41 @@ if run_simulation == "Y":
 
             elif ind == 3:
                 S = [1]
-                path_to_folder =  "Bands/" + str(run) + "/ISM/"
+                path_to_folder = "Bands/" + str(run) + "/ISM/"
                 print("ISM ------------------------ \n")
 
             elif ind == 2:
                 S = [2]
-                path_to_folder =   "Bands/" + str(run) + "/LTE/"
+                path_to_folder = "Bands/" + str(run) + "/LTE/"
                 print("LTE ----------------------------\n")
 
             elif ind == 4:
                 S = [3]
-                path_to_folder =   "Bands/" + str(run) + "/CBRS/"
+                path_to_folder = "Bands/" + str(run) + "/CBRS/"
                 print("CBRS --------------------------- \n")
 
+
+            #Set correct folder names
+            link_exists_folder = "Bands/" + str(run) + "/"
+            lex_data_directory = "Lexington/" + str(run) + "/"
+            lex_data_directory_day = "Lexington/" + str(run) + "/Day1/"
             validate_data_directory = "Lexington/" + str(run) + "/Day1/"
+
             with open("constants.py", "r") as f:
                 lines = f.readlines()
 
             with open("constants.py", "w") as f:
                 for line in lines:
-                    if ("path_to_folder" not in line and "S = " not in line and "validate_data_directory" not in line):
+                    if ("path_to_folder" not in line) and ("S = " not in line) and ("validate_data_directory" not in line) \
+                            and ("link_exists_folder" not in line) and ("lex_data_directory" not in line):
                         f.write(line)
 
                 f.write("path_to_folder = '" + str(path_to_folder) + "'\n")
                 f.write("S = " + str(S) + "\n")
                 f.write("validate_data_directory = '" + str(validate_data_directory) + "'\n")
+                f.write("lex_data_directory = '" + str(lex_data_directory) + "'\n")
+                f.write("lex_data_directory_day = '" + str(lex_data_directory_day) + "'\n")
+                f.write("link_exists_folder = '" + str(link_exists_folder) + "'\n")
 
 
             print("Round: " + str(run))
