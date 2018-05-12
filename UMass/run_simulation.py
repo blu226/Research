a9ca3edf0b94f597354e29951dfd67ec94c6373e
Brian Luciano <blu226@g.uky.edu>
2018-05-12 17:29:54 -0400

    Run all simulations

--- UMass/run_simulation.py
+++ UMass/run_simulation.py
@@ -7,33 +7,47 @@ def create_new_constants_file(day, V, T):
     os.system('rm constants.py')
     f = open("constants.py", "w")
     f.write("minX = 0\nmaxX = 100\nminY = 0\nmaxY = 100\nroute_start_time1 = 0\nroute_start_time2 = 15\ndt = 1\ntau = 1\n")
-    f.write("minBW = [3,8,20,40\nmaxBW = [6,20,30,60]\nspectRange = [2700,690,1800,540]\nspectPower = [1,1,1,1]\nepsilon = 0.5\n")
+    f.write("minBW = [3,8,20,40]\nmaxBW = [6,20,30,60]\nspectRange = [2700,690,1800,540]\nspectPower = [1,1,1,1]\nepsilon = 0.5\n")
     f.write("t_sd = 0.5\nt_td = 1\nidle_channel_prob = 0.5\nswitching_delay = 0.001\nsensing_power = 0.04\nlambda_val = 1\nmessageBurst = [2, 5]\n\n")
     f.write("link_exists_folder = 'Bands/'\nlex_data_directory = 'DataMules/2007-11-03_2007-11-04/'\n")
     f.write("NoOfSources = 6\nNoOfDataCenters = 3\n")
-    
+    f.write("TTL = 30\nminTTL=15\nmaxTau = 10\nM = [1,10,25,50,100,500,750,1000]\n")
     NoOfDMs = V - 9
-    delivery_file_name = "delivery_day" + str(day) +"_" + str(V) + "_" + str(T) + ".txt"
-    metrics_file_name = "metrics_LLC_day" +str(day)+"_" + str(V) + "_" + str(T)  + ".txt"
+    delivery_file_name = "delivery_file_name = \"delivery_day" + str(day) +"_" + str(V) + "_" + str(T) + ".txt\"\n"
+    metrics_file_name = "metrics_file_name = \"metrics_LLC_day" +str(day)+"_" + str(V) + "_" + str(T)  + ".txt\"\n"
     
-    f.write("NoOfDMs = " + str(NoOfDMs))
-    f.write("T = " + str(T) + "\n")
-    f.write("V = " + str(V) + "\n")
-    f.write("delivery_file_name = " + str(delivery_file_name))
-    f.write("metrics_file_name = " + str(metrics_file_name))
-    f.write("lex_data_directory_day = 'DataMules/2007-11-03_2007-11-04/Day" + str(day) + "/'\n")
-    f.write("validate_data_directory = 'DataMules/2007-11-03_2007-11-04/Day" + str(day) = "/'\n")
+    if day == 1:
+        dir1 = "lex_data_directory = \"DataMules/2007-11-03_2007-11-04/Day1/\"\n"
+        dir2 = "validate_data_directory = \"DataMules/2007-11-03_2007-11-04/Day1/\"\n"
+    else:
+        dir1 = "lex_data_directory = \"DataMules/2007-11-03_2007-11-04/Day2/\"\n"
+        dir2 = "validate_data_directory = \"DataMules/2007-11-03_2007-11-04/Day2/\"\n"
+
+
+    DM_line = "NoOfDMs = " + str(NoOfDMs) + "\n"
+    T_line = "T = " + str(T) + "\n"
+    V_line = "V = " + str(V) + "\n"
+
+
+    f.write(DM_line)
+    f.write(T_line)
+    f.write(V_line)
+    f.write(str(delivery_file_name))
+    f.write(str(metrics_file_name))
+    f.write(dir1)
+    f.write(dir2)
     f. write("StartTime = 850\n")
     f.close()
+    os.system('cat constants.py')
     
 def run_simulation_files(day, V, T):
-    
+    print("_________________________________________________") 
     print("Day: ", str(day), " V: ", str(V), " T: ", str(T))
 
     create_new_constants_file(day, V, T)
 
     if day == 1:
-        run = [0,1,2,3]
+        run = [0,1,2,3,4]
     else:
         run = [0]
 
