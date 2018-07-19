b9c9f0c8d46920d0a4e750ad54a50bd7dc6fc971
vksh224 <vijay.shah@uky.edu>
2018-07-19 16:03:05 -0400

    changed LLC to band with highest range

--- X-CHANTS_UMass/LLC_path.py
+++ X-CHANTS_UMass/LLC_path.py
@@ -53,10 +53,16 @@ def computeADJ_T_2(specBW, LINK_EXISTS):
                                 consumedEnergy = sensing_energy + switching_energy + transmission_energy
                                 consumedEnergy = round(consumedEnergy, 2)
 
-                                # print(i, j, t, consumedTime, m, specBW[i, j, s, t])
-                                if (t + consumedTime) < T  and LINK_EXISTS[i, j, s, t, (t + consumedTime)] < math.inf:
+                                if (t + consumedTime) < T and LINK_EXISTS[i, j, s, t, (t + consumedTime)] < math.inf:
 
-                                    if ADJ_T[i, j, t, m] > consumedTime or (ADJ_T[i, j, t, m] == consumedTime and spectRange[s] > spectRange[Spectrum[i, j, t, m] - 1]):
+                                    currSpec = Spectrum[i, j, t, m]
+                                    if currSpec > 9:
+                                        currSpec = currSpec % 10
+
+                                    currSpec = currSpec - 1
+
+                                    # if ADJ_T[i, j, t, m] > consumedTime or ((abs(ADJ_T[i, j, t, m] - consumedTime) < 1) and spectRange[s] > spectRange[currSpec]):
+                                    if ADJ_T[i, j, t, m] > consumedTime:
                                         ADJ_T[i, j, t, m] = consumedTime
                                         ADJ_E[i, j, t, m] = consumedEnergy
                                         Spectrum[i, j, t, m] = s + 1
