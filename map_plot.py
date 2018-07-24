957546fc3d9ece9f757bbf2d1d594816c2ccbc7e
vksh224 <vijay.shah@uky.edu>
2018-07-24 15:25:18 -0400

    committed map plot

--- map_plot.py
+++ map_plot.py
@@ -35,8 +35,8 @@ def readFile(fileName):
             lineStr = lineStr.split()
             # if count%2 == 0:
             # print(lineStr[3])
-            # if(float(lineStr[0]) >= 840) and float(lineStr[0]) <= 1080:
-            if (float(lineStr[0]) >= 660) and float(lineStr[0]) <= 840:
+            if(float(lineStr[0]) >= 840) and float(lineStr[0]) <= 1020:
+            # if (float(lineStr[0]) >= 660) and float(lineStr[0]) <= 840:
                 currPath.append((float(lineStr[2]), float(lineStr[3])))
 
                 count += 1
@@ -81,8 +81,8 @@ numOfFiles = len(currFiles)
 for fInd in range(0, numOfFiles):
     filePath = directory + "/" + currFiles[fInd]
 
-    # if currFiles[fInd] not in ["0.txt", "1.txt", "2.txt", "3.txt", "4.txt", "5.txt", "6.txt", "7.txt", "8.txt", "12.txt"]:
-    if currFiles[fInd] in ["17.txt"]:
+    # if currFiles[fInd]  not in ["0.txt", "1.txt", "2.txt", "3.txt", "4.txt", "5.txt", "6.txt", "7.txt", "8.txt"]:
+    if currFiles[fInd] in ["14.txt"]:
         currPath = readFile(filePath)
         allPaths.append(currPath)
 
@@ -94,7 +94,7 @@ gmap = gmplot.GoogleMapPlotter(42.393658, -72.53295, 12)
 
         # 0            1        2           3           4              5          6          7           8           9             10
           #Lime        Gold     Dark Red   Deep Pink  Forest Green   Blue       Black     Chocolate  Magneta  Royal blue  Brown
-colors = ['#00FF00', '#FFD700', '#8B0000', '#FF1493', '#228B22', '#0000FF', '#000000', '#A52A2A', '#FF0000', '#00008B'] #, '#8B008B', '#4169E1','#FF0000' ]
+colors = ['#00FF00', '#FFD700', '#8B0000', '#FF1493', '#228B22', '#0000FF', '#000000', '#A52A2A', '#FF0000', '#00008B', '#4169E1'] #, '#8B008B', '#4169E1','#FF0000' ]
 count = 0
 
 if not os.path.exists("HTML"):
@@ -106,11 +106,12 @@ for pInd in range(len(allPaths)):
     # if pInd == 1 or pInd == 4 or pInd == 6 : #or pInd == 6 or pInd > 0
     # print(str(pInd) + " " + str(len(allPaths[pInd])) + " " + str(allPaths[pInd]))
     path_lats, path_lons = zip(* allPaths[pInd])
-    colorInd = int(pInd%10)
+    colorInd = int(pInd)
     # print ("index: ", colorInd)
     gmap.scatter(path_lats, path_lons, colors[colorInd], size=60, marker=False)
 
+    # Draw
+    # gmap.draw(str(pInd) + "_" + "round2.html")
+    # os.chdir(curr)
 
-# Draw
-gmap.draw("round1.html")
-# os.chdir(curr)
+gmap.draw("14.html")
